e11291e3de4f3d56fd1df0ef0c98598419994e6d
Aron Birkir Gudmundsson <aron@idega.com>
2004-11-30 16:15:44 +0000

    Skipping filesystem copy

--- src/java/com/idega/slide/IWBundleStarter.java
+++ src/java/com/idega/slide/IWBundleStarter.java
@@ -1,5 +1,5 @@
 /*
- * $Id: IWBundleStarter.java,v 1.2 2004/11/17 12:35:40 aron Exp $
+ * $Id: IWBundleStarter.java,v 1.3 2004/11/30 16:15:44 aron Exp $
  * Created on 15.11.2004
  *
  * Copyright (C) 2004 Idega Software hf. All Rights Reserved.
@@ -16,15 +16,14 @@ import com.idega.business.IBOLookupException;
 import com.idega.idegaweb.IWApplicationContext;
 import com.idega.idegaweb.IWBundle;
 import com.idega.idegaweb.IWBundleStartable;
-import com.idega.slide.business.FileSystemCopyService;
 import com.idega.slide.business.IWSlideService;
 
 /**
  * 
- *  Last modified: $Date: 2004/11/17 12:35:40 $ by $Author: aron $
+ *  Last modified: $Date: 2004/11/30 16:15:44 $ by $Author: aron $
  * 
  * @author <a href="mailto:aron@idega.com">aron</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class IWBundleStarter implements IWBundleStartable {
 
@@ -43,17 +42,7 @@ public class IWBundleStarter implements IWBundleStartable {
         } catch (RemoteException e) {
             e.printStackTrace();
         }
-        
-      
-    	    try {
-                ((FileSystemCopyService)IBOLookup.getServiceInstance(iwac,FileSystemCopyService.class)).run();
-            } catch (IBOLookupException e) {
-                e.printStackTrace();
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+       
 
     }
     /* (non-Javadoc)
