ed4f8d56f19887bf5ad7500f47d0b8bb47f32198
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2005-09-14 23:34:38 +0000

    Fixed so that versions are ordered correctly (1.9<1.10 e.g.)

--- src/java/com/idega/slide/util/VersionHelper.java
+++ src/java/com/idega/slide/util/VersionHelper.java
@@ -80,7 +80,8 @@ public class VersionHelper {
 	}
 	
 	/**
-	 * 
+	 * <p>
+	 * Returns a list of version for resource in Descending (e.g. 1.11,1.10,1.9,1.8,1.7,1.6,1.5,1.4,.1.3,1.2,1.1,1.0) order
 	 * @param resource
 	 * @return A list of WebdavResourceVersion
 	 */
--- src/java/com/idega/slide/util/WebdavResourceVersion.java
+++ src/java/com/idega/slide/util/WebdavResourceVersion.java
@@ -1,5 +1,5 @@
 /*
- * $Id: WebdavResourceVersion.java,v 1.4 2005/01/10 11:36:00 gimmi Exp $ Created on Dec
+ * $Id: WebdavResourceVersion.java,v 1.5 2005/09/14 23:34:38 tryggvil Exp $ Created on Dec
  * 19, 2004
  * 
  * Copyright (C) 2004 Idega Software hf. All Rights Reserved.
@@ -20,12 +20,12 @@ import com.idega.user.data.User;
 
 /**
  * 
- * Last modified: $Date: 2005/01/10 11:36:00 $ by $Author: gimmi $
+ * Last modified: $Date: 2005/09/14 23:34:38 $ by $Author: tryggvil $
  * 
  * A little wrapper for version information
  * 
  * @author <a href="mailto:eiki@idega.com">Eirikur S. Hrafnsson </a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public class WebdavResourceVersion implements Comparable{
 
@@ -147,7 +147,52 @@ public class WebdavResourceVersion implements Comparable{
 	 * @see java.lang.Comparable#compareTo(java.lang.Object)
 	 */
 	public int compareTo(Object version) {
-		return Collator.getInstance().compare(this.toString(),version.toString());
+		String otherVersionToString = version.toString();
+		String thisVersionToString = this.toString();
+		
+		int otherIndexOfDot = otherVersionToString.indexOf(".");
+		int thisIndexOfDot = thisVersionToString.indexOf(".");
+		if(otherIndexOfDot!=-1 && thisIndexOfDot!=-1){
+			//Cut of the first dot, to make comparision handle the cases 1.9 and 1.10 e.g.
+			String otherVersionToStringBeforeDot = otherVersionToString.substring(0,otherIndexOfDot);
+			String thisVersionToStringBeforeDot = thisVersionToString.substring(0,thisIndexOfDot);
+			
+			String otherVersionToStringAfterDot = otherVersionToString.substring(otherIndexOfDot+1,otherVersionToString.length());
+			String thisVersionToStringAfterDot = thisVersionToString.substring(thisIndexOfDot+1,thisVersionToString.length());
+			try{
+				int intOtherVersionBeforeDot = Integer.parseInt(otherVersionToStringBeforeDot);
+				int intThisVersionBeforeDot = Integer.parseInt(thisVersionToStringBeforeDot);
+				
+				int intOtherVersionAfterDot = Integer.parseInt(otherVersionToStringAfterDot);
+				int intThisVersionAfterDot = Integer.parseInt(thisVersionToStringAfterDot);
+				
+				if(intOtherVersionBeforeDot==intThisVersionBeforeDot){
+					if(intOtherVersionAfterDot==intThisVersionAfterDot){
+						return 0;
+					}
+					else if(intOtherVersionAfterDot>intThisVersionAfterDot){
+						return -1;
+					}
+					else if(intOtherVersionAfterDot<intThisVersionAfterDot){
+						return 1;
+					}
+				}
+				else if(intOtherVersionBeforeDot>intThisVersionBeforeDot){
+					return -1;
+				}
+				else if(intOtherVersionBeforeDot<intThisVersionBeforeDot){
+					return 1;
+				}
+				
+			}
+			catch(NumberFormatException nfe){
+				nfe.printStackTrace();
+			}
+		
+		}
+		
+		int theReturn = Collator.getInstance().compare(thisVersionToString,otherVersionToString);
+		return theReturn;
 	}
 	
 	public String getCreationDate() {
