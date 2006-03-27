751292913a728523d257a486183b08ff21b7e98a
Eirikur Hrafnsson <eiki@idega.com>
2006-03-27 14:42:21 +0000

    Adding image property extractor that save width height and more info about images in their metadata.Also adding ConstantExtractor that lets you specify property in Domain.xml to always set on files (in a specific directory)

---
+++ src/java/com/idega/slide/extractor/ConstantExtractor.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2004 Hippo Webworks.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.idega.slide.extractor;
+
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.slide.common.PropertyName;
+import org.apache.slide.extractor.AbstractPropertyExtractor;
+import org.apache.slide.extractor.ExtractorException;
+import org.apache.slide.util.conf.Configurable;
+import org.apache.slide.util.conf.Configuration;
+import org.apache.slide.util.conf.ConfigurationException;
+
+/**
+ * 
+ * Puts constant properties on a document. Not really an extractor but handy.
+ * With this extractor you could set a fixed property on all files within a certain folder.
+ * You configure this extractor in Domain.xml.
+ * 
+ * @author <a href="mailto:m.pfingsthorn@hippo.nl">Max Pfingsthorn</a>
+ */
+public class ConstantExtractor extends AbstractPropertyExtractor implements Configurable
+{
+	private Map properties = new HashMap();
+    
+    public ConstantExtractor(String namespace, String uri, String contentType) {
+        super(namespace, uri, contentType);
+    }
+
+   
+
+    public Map extract(InputStream content) throws ExtractorException
+	{
+		return properties;
+	}
+    
+    public void configure(Configuration configuration) throws ConfigurationException {
+        Enumeration instructions = configuration.getConfigurations("instruction");
+        
+        
+        while (instructions.hasMoreElements()) {
+            Configuration instruction = (Configuration)instructions.nextElement();
+            PropertyName propertyName = new PropertyName(
+            		instruction.getAttribute("property"),
+					instruction.getAttribute("namespace", "DAV:"));
+            
+            String value = instruction.getAttribute("value","");
+            
+            properties.put(propertyName,value);
+        }
+	}
+}
---
+++ src/java/com/idega/slide/extractor/ImagePropertyExtractor.java
@@ -0,0 +1,48 @@
+package com.idega.slide.extractor;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.slide.extractor.AbstractPropertyExtractor;
+import org.apache.slide.extractor.ExtractorException;
+import com.idega.graphics.ImageInfo;
+import com.idega.slide.util.IWSlideConstants;
+
+/**
+ * Extracts image information; height,width,bits per pixel and dpi. <br>
+ * Based on the open source projects ImageInfo and code from Hippo CMS
+ * 
+ * @author <a href="mailto:eiki@idega.is">Eirikur Hrafnsson</a>
+ */
+public class ImagePropertyExtractor extends AbstractPropertyExtractor {
+
+	private static final String HEIGHT_PROPERTY = IWSlideConstants.PROPERTYNAME_HEIGHT;
+	private static final String WIDTH_PROPERTY = IWSlideConstants.PROPERTYNAME_WIDTH;
+	private static final String BITS_PER_PIXEL_PROPERTY = IWSlideConstants.PROPERTYNAME_BITS_PER_PIXEL;
+	private static final String DPI_PROPERTY = IWSlideConstants.PROPERTYNAME_DPI;
+	private static final String WIDTH_AND_HEIGHT_PROPERTY = IWSlideConstants.PROPERTYNAME_WIDTH_AND_HEIGHT_PROPERTY;
+
+	public ImagePropertyExtractor(String namespace, String uri, String contentType) {
+		super(namespace, uri, contentType);
+	}
+
+	public Map extract(InputStream content) throws ExtractorException {
+		Map result = new HashMap();
+		ImageInfo ii = new ImageInfo();
+		ii.setInput(content);
+		if (ii.check()) {
+			final int width = ii.getWidth();
+			final int height = ii.getHeight();
+			final int bitsPerPixel = ii.getBitsPerPixel();
+			final int widthDpi = ii.getPhysicalWidthDpi();
+			result.put(HEIGHT_PROPERTY, String.valueOf(height));
+			result.put(WIDTH_PROPERTY, String.valueOf(width));
+			result.put(WIDTH_AND_HEIGHT_PROPERTY, String.valueOf(width) + "x" + String.valueOf(height));
+			result.put(BITS_PER_PIXEL_PROPERTY, String.valueOf(bitsPerPixel));
+			if (widthDpi != -1) {
+				result.put(DPI_PROPERTY, String.valueOf(widthDpi));
+			}
+		}
+		return result;
+	}
+}
