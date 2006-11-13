6d73f4c1cdcb67e6984f174a3bb19a868d68680a
Grimur Jonsson <gimmi@idega.com>
2006-11-13 11:50:40 +0000

    PropertyHandler for IWSlideProperties

---
+++ src/java/com/idega/slide/presentation/IWSlidePropertyHandler.java
@@ -0,0 +1,40 @@
+package com.idega.slide.presentation;
+
+import java.util.List;
+
+import com.idega.core.builder.presentation.ICPropertyHandler;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.PresentationObject;
+import com.idega.presentation.ui.DropdownMenu;
+import com.idega.slide.util.IWSlideConstants;
+
+public class IWSlidePropertyHandler implements ICPropertyHandler {
+
+	public List getDefaultHandlerTypes() {
+		return null;
+	}
+
+	public PresentationObject getHandlerObject(String name, String stringValue, IWContext iwc) {
+		DropdownMenu menu = new DropdownMenu(name);
+		
+		try {
+			menu.addMenuElement(IWSlideConstants.PROPERTYNAME_DISPLAY_NAME, "Name");
+			menu.addMenuElement(IWSlideConstants.PROPERTYNAME_CONTENT_TYPE, "Type");
+			menu.addMenuElement(IWSlideConstants.PROPERTYNAME_CONTENT_LENGTH, "Size");
+			menu.addMenuElement(IWSlideConstants.PROPERTYNAME_LAST_MODIFIED, "Modification Date");
+			menu.addMenuElement(IWSlideConstants.PROPERTYNAME_CREATION_DATE, "Creation Date");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		if (stringValue != null) {
+			menu.setSelectedElement(stringValue);
+		}
+		return menu;
+	}
+
+	public void onUpdate(String[] values, IWContext iwc) {
+	}
+
+}
