e31b0d66752a060bbcae47e9ec087b0d8716b5e5
Valdas Zemaitis <valdas@idega.com>
2009-06-11 12:33:20 +0000

    New constant

--- src/java/com/idega/slide/SlideConstants.java
+++ src/java/com/idega/slide/SlideConstants.java
@@ -5,4 +5,6 @@ public class SlideConstants {
 	public static final String SIMPLE_SLIDE_SERVICE = "iwSimpleSlideService";
 	public static final String BUNDLE_IDENTIFIER = "com.idega.slide";
 	
+	public static final String SLIDE_SCHEME = "slide";
+	
 }
