ea2bd4ae651639e8df0c863aa31dd56b32f1d027
Valdas Žemaitis <valdas@idega.com>
2010-02-06 16:47:54 +0100

    Testing sibling/descendant uploads too (100 threads starts uploading at the same time...)

--- src/java/com/idega/slide/upload/UploadTest.java
+++ src/java/com/idega/slide/upload/UploadTest.java
@@ -1,5 +1,9 @@
 package com.idega.slide.upload;
 
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,49 +14,58 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import com.idega.core.business.DefaultSpringBean;
+import com.idega.dwr.business.DWRAnnotationPersistance;
 import com.idega.idegaweb.IWMainSlideStartedEvent;
 import com.idega.slide.business.IWSlideService;
 import com.idega.util.StringHandler;
 
 @Service
 @Scope(BeanDefinition.SCOPE_SINGLETON)
-public class UploadTest extends DefaultSpringBean implements ApplicationListener {
-
+public class UploadTest extends DefaultSpringBean implements ApplicationListener, DWRAnnotationPersistance {
+	
 	private Logger LOGGER;
+
+	private Random random;
 	
 	public void onApplicationEvent(ApplicationEvent event) {
 		if (event instanceof IWMainSlideStartedEvent) {
 			IWMainSlideStartedEvent slideStartedEvent = (IWMainSlideStartedEvent) event;
 			if (slideStartedEvent.getIWMA().getSettings().getBoolean("test_mass_uploads", Boolean.FALSE)) {
 				LOGGER = LOGGER == null ? Logger.getLogger(UploadTest.class.getName()) : LOGGER;
-				final IWSlideService slide = getServiceInstance(slideStartedEvent.getIWMA().getIWApplicationContext(), IWSlideService.class);
+				random = random == null ? new Random() : random;
 				
-				final String uploadPath = "/files/public/mass_uploads/";
-				try {
-					if (slide.getExistence(uploadPath)) {
-						slide.deleteAsRootUser(uploadPath);
-					}
-				} catch (Exception e) {
-					LOGGER.log(Level.WARNING, "Error while deleting: " + uploadPath, e);
-				}
-				
-				final String fileName = "File_";
-				for (int i = 0; i < 100; i++) {
-					final int fileNumber = i+1;
-					Thread worker = new Thread(new Runnable() {
-						public void run() {
-							try {
-								boolean result = slide.uploadFile(uploadPath, fileName + fileNumber + ".txt", "text/plain",
-										StringHandler.getStreamFromString("File " + fileNumber + " with no content"));
-								LOGGER.info("Uploaded file: " + fileNumber + ": " + result);
-							} catch (Exception e) {
-								LOGGER.log(Level.WARNING, "Error while uploading file: " + fileNumber, e);
-							}
-						}
-					});
-					worker.start();
-				}
+				executeUploads();
 			}
 		}
+	}
+	
+	public void executeUploads() {
+		final IWSlideService slide = getServiceInstance(IWSlideService.class);
+		
+		String mainDir = "/files/public/concurent_uploads_test/";
+		String subDir1 = mainDir.concat("t1/");
+		final List<String> uploadPaths = Arrays.asList(mainDir, subDir1, mainDir.concat("t2/"), subDir1.concat("sub/"));		
+		final String fileName = "File_";
+		for (int i = 0; i < 100; i++) {
+			final int fileNumber = i+1;
+			Thread worker = new Thread(new Runnable() {
+				public void run() {
+					String path = getRandomPath(uploadPaths);
+					String name = fileName.concat(String.valueOf(fileNumber)).concat(".txt");
+					try {
+						InputStream stream = StringHandler.getStreamFromString("File " + fileNumber + " with no content in folder: " + path);
+						boolean result = slide.uploadFile(path, name, "text/plain", stream);
+						LOGGER.info("Uploaded file: " + path + name + ": " + result);
+					} catch (Exception e) {
+						LOGGER.log(Level.WARNING, "Error while uploading file: " + path + name, e);
+					}
+				}
+			});
+			worker.start();
+		}
+	}
+
+	private String getRandomPath(List<String> paths) {
+		return paths.get(random.nextInt(paths.size()));
 	}
 }
